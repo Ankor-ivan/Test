e0274e2d1d8965e6030475ab8e83a251c31a3f48
ivan <ankor-ivan@mail.ru>
2019-04-14 12:16:11 +0300

    это на первый комит

---
+++ index1.js
@@ -0,0 +1,29 @@
+'usestrict';
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+
+let arr = ['plum.png','orange.jpg','apple.bmp']
+console.log(arr[2])
+//alert('Hello World!');
+//let answer = confirm('are you here?');
+//console.log(answer);
+
+//let answer = +prompt('Есть ли Вам 18?', 'да');
+//console.log(typeof(answer));
+//console.log('arr' + ' - object');
+//console.log('4' + ' - object');
+let incr = 10,
+    decr = 10;
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+
+let isCheked = false,
+    isClose = false;
+    console.log(isCheked || !isClose);
