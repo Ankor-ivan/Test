ee9b00fb171ffc4d479731695c6d16159aa61ec4
unknown <ankor-ivan@mail.ru>
2019-10-30 15:55:28 +0300

    first

--- index1.js
+++ index1.js
@@ -27,3 +27,54 @@ console.log("2" === 2);
 let isCheked = false,
     isClose = false;
     console.log(isCheked || !isClose);
+
+    if(2*4 == 9){
+console.log("верно!")
+
+    } else{
+        console.log("неверно!")
+    }
+
+
+    let num = 50;
+
+    if (num < 49) {
+        console.log("неверно!")
+    } else if(num > 100) {
+       console.log("Много!")
+    } else{
+        console.log("Верно!")
+    };
+
+    switch (num) {
+        case num < 49:
+              console.log("неверно!");
+              break;
+        case num > 100:
+              console.log("Много!");
+              break;
+        case num > 80:
+                console.log("Все еще Много!");   
+              break;
+        case 50:
+                console.log("Верно!");
+                break;
+    }
+    let num = 50;
+    //while (num < 55){
+    //    console.log(num);
+    //    num++;
+        
+    //}
+    do{
+        console.log(num);
+        num++; 
+    }
+    while (num < 55);
+
+    for (let i = 1; i < 8; i++){
+        if (i == 6){
+        break
+        }
+        console.log(i);
+    }
